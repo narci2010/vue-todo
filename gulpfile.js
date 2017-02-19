d0adeca9e92be3295095f45492e687d16f12e860
Daniel Hoeflehner <daniel.hoeflehner@outlook.com>
2017-02-19 16:55:21 +0100

    reorder folders

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ let sass = require('gulp-sass');
 gulp.task('sass', function () {
     return gulp.src('./assets/style.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function () {
